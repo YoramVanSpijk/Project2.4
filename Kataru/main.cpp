e79fff12a7aefd4fd504509497691ecfaacecfef
Yoram van Spijk <yoramvanspijk1994@hotmail.com>
2020-06-03 15:32:55 +0200

    2D Text can be displayed in the 3D world

--- Kataru/main.cpp
+++ Kataru/main.cpp
@@ -6,7 +6,16 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#define STB_TRUETYPE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "tigl.h"
+#include "stb_truetype.h"
+#include "stb_image_write.h"
+#include "stb_image_resize.h"
+#include "stb_image.h"
 #include "FpsCam.h"
 #include "GameObject.h"
 #include "CameraObject.h"
@@ -18,7 +27,9 @@
 #pragma comment(lib, "opencv_world341d.lib")
 
 GLFWwindow* window;
+GLuint texId;
 double lastFrameTime;
+stbtt_bakedchar cdata[96];
 
 std::vector<CameraObject*> cameraObjects;
 std::vector<GameObject*> gameObjects;
@@ -26,6 +37,8 @@ std::vector<GameObject*> gameObjects;
 void init();
 void update();
 void draw();
+void initTrueType();
+void drawText(float, float, std::string);
 
 int main(void)
 {
@@ -42,7 +55,8 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     tigl::init();
-    init();
+    initTrueType();
+    init();  
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -109,9 +123,58 @@ void draw()
     glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    drawText(0, 0, "Test");
+    drawText(150, 0, "Test iets meer naar rechts");
+
     for (size_t i = 0; i < cameraObjects.size(); i++)
         cameraObjects[i]->draw();
 
     for (size_t i = 0; i < gameObjects.size(); i++)
         gameObjects[i]->draw();
+}
+
+void initTrueType() {
+    unsigned char* ttf_buffer = new unsigned char[1 << 20];
+    unsigned char* temp_bitmap = new unsigned char[512 * 512];
+    fread(ttf_buffer, 1, 1 << 20, fopen("c:/windows/fonts/times.ttf", "rb"));
+    stbtt_BakeFontBitmap(ttf_buffer, 0, 32.0, temp_bitmap, 512, 512, 32, 96, cdata);
+    glGenTextures(1, &texId);
+    glBindTexture(GL_TEXTURE_2D, texId);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, 512, 512, 0, GL_ALPHA, GL_UNSIGNED_BYTE, temp_bitmap);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    delete[] ttf_buffer;
+    delete[] temp_bitmap;
+}
+
+void drawText(float x, float y, std::string text) {
+    /*int viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    tigl::shader->setProjectionMatrix(glm::ortho(0.0f, (float)viewport[2], (float)viewport[3], 0.0f, -100.0f, 100.0f));
+    tigl::shader->setViewMatrix(glm::mat4(1.0f));
+    tigl::shader->setModelMatrix(glm::mat4(1.0f));
+
+    tigl::shader->enableColor(false);
+    tigl::shader->enableLighting(false);
+    tigl::shader->enableTexture(true);
+    tigl::shader->enableColorMult(false);*/
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_DEPTH_TEST);
+    glBindTexture(GL_TEXTURE_2D, texId);
+
+    stbtt_aligned_quad q;
+    tigl::begin(GL_QUADS);
+    for (int i = 0; i < text.size(); i++) {
+        if (text[i] >= 32 && text[i] < 128) {
+            stbtt_GetBakedQuad(cdata, 512, 512, text[i] - 32, &x, &y, &q, 1);
+            tigl::addVertex(tigl::Vertex::PT(glm::vec3(q.x0, q.y0, 0), glm::vec2(q.s0, q.t0)));
+            tigl::addVertex(tigl::Vertex::PT(glm::vec3(q.x1, q.y0, 0), glm::vec2(q.s1, q.t0)));
+            tigl::addVertex(tigl::Vertex::PT(glm::vec3(q.x1, q.y1, 0), glm::vec2(q.s1, q.t1)));
+            tigl::addVertex(tigl::Vertex::PT(glm::vec3(q.x0, q.y1, 0), glm::vec2(q.s0, q.t1)));
+        }
+    }
+    tigl::end();
+
 }

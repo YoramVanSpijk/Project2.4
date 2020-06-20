cad6031f120eeb22308cbbd4dc226dcbcfe1166e
Wout <woutstevens@live.nl>
2020-06-20 15:58:29 +0200

    Deleted a random println

--- Kataru/ObjSpawner.cpp
+++ Kataru/ObjSpawner.cpp
@@ -85,7 +85,6 @@ double ObjSpawner::fRand(double fMin, double fMax)
 {
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
-    printf("");
 }
 
 void ObjSpawner::update(double deltaTime)

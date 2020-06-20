3363ab64ba25b46b771c33996f222e2705530136
Jayson <mjaysondeleeuw@gmail.com>
2020-06-20 20:26:07 +0200

    [Add/Fix] Feedback, CV > OPENGL coords, collision, ROI detect and hitbox

--- Kataru/VisionCamera.cpp
+++ Kataru/VisionCamera.cpp
@@ -27,6 +27,8 @@ VisionCamera::VisionCamera(GLFWwindow* window)
     this->params.filterByArea = true;
     this->params.minArea = minAreaBlob;
     this->params.maxArea = maxAreaBlob;
+
+	cv::namedWindow(MASK_TAG, CV_WINDOW_NORMAL);
 }
 
 
@@ -59,8 +61,8 @@ void VisionCamera::draw()
 		flip(frame, frame, 3);
 
 		// Size of ROI for color selecting
-		cv::Rect rLeft = cv::Rect((frame.size().width / 2) - 10, (frame.size().height / 2) - 20, 20, 40);
-		cv::Rect rRight = cv::Rect((frame.size().width / 2) + 10, (frame.size().height / 2) - 20, 20, 40);
+		cv::Rect rLeft((frame.size().width / 2) - 20, (frame.size().height / 2) - 20, 40, 40);
+		cv::Rect rRight((frame.size().width / 2) + 10, (frame.size().height / 2) - 20, 40, 40);
 
 		// Cropping ROI from main frame
 		croppedImageL = frame(rLeft);
@@ -89,10 +91,12 @@ void VisionCamera::draw()
 			std::vector<cv::KeyPoint> keypoints;
 			cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
 			detector->detect(mask, keypoints);
-			drawKeypoints(frame, keypoints, frame, cv::Scalar(255, 0, 0), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+			// Draw around object
+			//drawKeypoints(frame, keypoints, frame, cv::Scalar(0, 255, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
 			for (int i = 0; i < keypoints.size(); i++) {
-				cv::Point2f pRaw = cv::Point2f(keypoints[i].pt.x - (frame.size().width / 2), keypoints[i].pt.y - (frame.size().height / 2));
+				cv::Point2f pRaw(keypoints[i].pt.x - (frame.size().width / 2), keypoints[i].pt.y - (frame.size().height / 2));
 
 				if (pRaw.y < 0) {
 					pRaw.y -= pRaw.y * 2;
@@ -101,12 +105,10 @@ void VisionCamera::draw()
 					pRaw.y *= -1;
 				}
 
-				cv::Point2f pFormat = cv::Point2f(pRaw.x, pRaw.y);
-				
-				std::cout << "X: " << pFormat.x << ", Y: " << pFormat.y << "\n";
+				cv::Point2f pFormat(pRaw.x, pRaw.y);
 				this->currentPoint = cv::Point2f(pFormat.x, pFormat.y);
 
-				circle(frame, keypoints[i].pt, 4, cv::Scalar(0, 0, 255), -1, 8, 0);
+				circle(frame, keypoints[i].pt, 25, cv::Scalar(0, 255, 255), -1, 6, 0);
 			}
 		}
 
@@ -130,18 +132,18 @@ void VisionCamera::draw()
 			vHighThreshold = cv::max(hsvMeansSample1[2], hsvMeansSample2[2]) + offsetHighThreshold;
 			vLowThreshold = cv::min(hsvMeansSample1[2], hsvMeansSample2[2]) - offsetLowThreshold;
 
-			// Create window for controls
-			cv::namedWindow("Controls", CV_WINDOW_NORMAL);
+			//// Create window for controls DEBUG
+			//cv::namedWindow("Controls", CV_WINDOW_NORMAL);
 
-			// Create all trackbars that the user can control to perfect the output
-			cv::createTrackbar("hMin", CONTROLS_TAG, &hLowThreshold, 179);
-			cv::createTrackbar("hMax", CONTROLS_TAG, &hHighThreshold, 179);
-			cv::createTrackbar("sMin", CONTROLS_TAG, &sLowThreshold, 255);
-			cv::createTrackbar("sMax", CONTROLS_TAG, &sHighThreshold, 255);
-			cv::createTrackbar("vMin", CONTROLS_TAG, &vLowThreshold, 255);
-			cv::createTrackbar("vMax", CONTROLS_TAG, &vHighThreshold, 255);
-			cv::createTrackbar("bMin", CONTROLS_TAG, &minAreaBlob, 30000);
-			cv::createTrackbar("bMax", CONTROLS_TAG, &maxAreaBlob, 50000);
+			//// Create all trackbars that the user can control to perfect the output
+			//cv::createTrackbar("hMin", CONTROLS_TAG, &hLowThreshold, 179);
+			//cv::createTrackbar("hMax", CONTROLS_TAG, &hHighThreshold, 179);
+			//cv::createTrackbar("sMin", CONTROLS_TAG, &sLowThreshold, 255);
+			//cv::createTrackbar("sMax", CONTROLS_TAG, &sHighThreshold, 255);
+			//cv::createTrackbar("vMin", CONTROLS_TAG, &vLowThreshold, 255);
+			//cv::createTrackbar("vMax", CONTROLS_TAG, &vHighThreshold, 255);
+			//cv::createTrackbar("bMin", CONTROLS_TAG, &minAreaBlob, 30000);
+			//cv::createTrackbar("bMax", CONTROLS_TAG, &maxAreaBlob, 50000);
 
 			this->colorDetected = true;
 		}
@@ -154,14 +156,14 @@ void VisionCamera::draw()
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	}
 
-	glm::mat4 camModel = glm::mat4(1.0f);
-	camModel = glm::translate(camModel, glm::vec3(-1.0f, -0.5f, -1.0f));
+	auto camModel = glm::mat4(1.0f);
+	camModel = glm::translate(camModel, glm::vec3(-1.0f, -0.55f, -1.0f));
 	tigl::shader->setModelMatrix(camModel);
 
 	tigl::begin(GL_QUADS);
 	tigl::addVertex(tigl::Vertex::PT(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec2(0, 0)));
 	tigl::addVertex(tigl::Vertex::PT(glm::vec3(2.0f, 0.0f, 0.0f), glm::vec2(1, 0)));
-	tigl::addVertex(tigl::Vertex::PT(glm::vec3(2.0f, 1.0f, 0.0f), glm::vec2(1, -1)));
-	tigl::addVertex(tigl::Vertex::PT(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0, -1)));
+	tigl::addVertex(tigl::Vertex::PT(glm::vec3(2.0f, 1.1f, 0.0f), glm::vec2(1, -1)));
+	tigl::addVertex(tigl::Vertex::PT(glm::vec3(0.0f, 1.1f, 0.0f), glm::vec2(0, -1)));
 	tigl::end();
 }
--- Kataru/main.cpp
+++ Kataru/main.cpp
@@ -26,6 +26,7 @@ GameStateHandler::GameState currentGameState;
 
 std::vector<GuiObject*> guiObjects;
 std::vector<GameObject*> gameObjects;
+VisionCamera* vc;
 
 const char* glsl_version = "#version 130";
 void initImGui();
@@ -118,7 +119,7 @@ void init()
     cursorChangeMenu = true;
     cursorChangeGame = true;
     lastFrameTime = 0;
-    attachGameObject(nullptr, new VisionCamera(window), glm::vec3(0.0f, 0.0f, 0.0f));
+    attachGameObject(nullptr, vc = new VisionCamera(window), glm::vec3(0.0f, 0.0f, 0.0f));
     attachGuiObject(nullptr, window, new MenuGuiComponent(gameStateHandler));
 }
 
@@ -177,6 +178,46 @@ void update()
     }
 }
 
+void drawCircleHitbox(float cx, float cy, float r)
+{
+    auto const segments = 12;
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glLineWidth(2.0f);
+
+    auto hitboxModel = glm::mat4(1.0f);
+    hitboxModel = glm::scale(hitboxModel, glm::vec3(0.05f, 0.05f, 1.0f));
+    hitboxModel = glm::translate(hitboxModel, glm::vec3(cx, cy, -1.0f));
+    tigl::shader->setModelMatrix(hitboxModel);
+    tigl::shader->enableTexture(false);
+    tigl::shader->enableColorMult(true);
+
+    tigl::begin(GL_LINE_LOOP);
+    for (int ii = 0; ii < segments; ii++)
+    {
+        float theta = 2.0f * 3.1415926f * float(ii) / float(segments); // get the current angle 
+
+        float x = r * cosf(theta); // calculate the x component 
+        float y = r * sinf(theta); // calculate the y component 
+
+        tigl::shader->setColorMult(glm::vec4(1.0f, 0.5f, 0.0f, 1.0f)); // Orange
+        tigl::addVertex(tigl::Vertex::P(glm::vec3(x + cx, y + cy, 0.0f)));
+    }
+    tigl::end();
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    tigl::shader->enableColorMult(false);
+}
+
+struct Circle
+{
+    float r;
+    float x;
+    float y;
+};
+
+float xCoordH = 0.0f;
+float yCoordH = 0.0f;
+
 void draw()
 {
     int width, height;
@@ -189,6 +230,23 @@ void draw()
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    // Move object
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+        xCoordH -= 0.1f;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+        xCoordH += 0.1f;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        yCoordH += 0.1f;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        yCoordH -= 0.1f;
+    }
+
     switch (currentGameState)
     {
         case GameStateHandler::GameState::Menu:
@@ -197,6 +255,37 @@ void draw()
 
             break;
         case GameStateHandler::GameState::Game:
+
+            // Get current point
+            cv::Point2f handCoord(vc->getCurrentPoint());
+            std::cout << "hX: " << handCoord.x << ", hY: " << handCoord.y << "\n";
+
+            // struct hitboxes for objects
+            struct Circle handHitBox, objectHitBox;
+            handHitBox.r = 15.0f;
+            handHitBox.x = handCoord.x;
+            handHitBox.y = handCoord.y;
+
+            objectHitBox.r = 15.0f;
+            objectHitBox.x = (xCoordH + 0.15f) * 25;
+            objectHitBox.y = yCoordH * (200 / 6);
+
+            std::cout << "oX: " << objectHitBox.x << ", oY: " << objectHitBox.y << "\n";
+
+            // Calculate distance between hitboxes
+            auto dx = handHitBox.x - objectHitBox.x;
+            auto dy = handHitBox.y - objectHitBox.y;
+            auto distance = sqrt(dx * dx + dy * dy);
+
+            // Check collision
+            if (distance < handHitBox.r + objectHitBox.r) {
+                std::cout << "COL\n";
+            }
+
+            // Draw hitbox
+            drawCircleHitbox((xCoordH + 0.15f), yCoordH, objectHitBox.r / 10);
+
+            // Draw objects
             for (size_t i = 0; i < gameObjects.size(); i++)
                 gameObjects[i]->draw();
 
